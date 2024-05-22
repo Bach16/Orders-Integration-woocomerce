5665893be470d90c750a2f02b7e60e5cc957b646
genesisbcde <gen10x100@gmail.com>
2024-05-22 15:32:02 -0500

    responsive del login

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,26 +1,24 @@
 <template>
   <v-row class="fill-height" justify="center" align="center">
     <v-col cols="12" lg="6" md="8" sm="10">
-      <v-card ref="form">
+      <v-card ref="form" class="pa-4 mx-2">
         <h1 class="text-center pa-2">Empresa Logo</h1>
-        <v-form class="" align="center" justify="center">
+        <v-form class="text-center">
           <v-container fluid>
             <v-row align="center" justify="center">
-              <v-col cols="12" sm="6">
+              <v-col cols="12" sm="10" md="8">
                 <v-text-field
-                  width="100%"
                   label="User"
                   :rules="[rules.required]"
                 ></v-text-field>
               </v-col>
             </v-row>
             <v-row align="center" justify="center">
-              <v-col cols="12" sm="6">
+              <v-col cols="12" sm="10" md="8">
                 <v-text-field
                   :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
                   :rules="[rules.required]"
                   :type="show2 ? 'text' : 'password'"
-                  class="input-group--focused"
                   label="Password"
                   name="input-10-2"
                   @click:append="show2 = !show2"
@@ -30,9 +28,8 @@
           </v-container>
         </v-form>
 
-        <v-divider class="mt-12"></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
+        <v-divider class="mt-4"></v-divider>
+        <v-card-actions class="d-flex justify-end">
           <v-tooltip v-if="formHasErrors" location="left">
             <template v-slot:activator="{ on, attrs }">
               <v-btn
@@ -48,9 +45,7 @@
             <span>Refresh form</span>
           </v-tooltip>
           <RouterLink
-            :to="{
-              name: 'searchOrder',
-            }"
+            :to="{ name: 'searchOrder' }"
           >
             <v-btn color="primary" variant="text" @click="submit">
               Ingresar
@@ -66,13 +61,9 @@
 export default {
   data() {
     return {
-      show1: false,
       show2: false,
-      password: "",
       rules: {
         required: (value) => !!value || "Required.",
-        min: (v) => v.length >= 8 || "Min 8 characters",
-        emailMatch: () => `The email and password you entered don't match`,
       },
     };
   },
@@ -83,7 +74,11 @@ export default {
 .fill-height {
   min-height: 100vh;
 }
-.mt-12 {
-  margin-top: 12px;
+.mt-4 {
+  margin-top: 16px;
+}
+.mx-2 {
+  margin-left: 8px;
+  margin-right: 8px;
 }
 </style>
