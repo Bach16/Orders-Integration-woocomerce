fd1f22531a50dbf1816ac54aa779ff45e94111dc
Vale <vmgomeza09@gmail.com>
2024-05-22 11:36:59 -0500

    Ventana de dialogo

--- src/views/DeliveredOrder.vue
+++ src/views/DeliveredOrder.vue
@@ -23,15 +23,7 @@
           />
         </v-col>
         <v-col cols="4">
-          <v-btn
-            class="ml-5"
-            color="primary"
-            :loading="loading"
-            @click="onClick"
-            height="56"
-          >
-            Buscar
-          </v-btn>
+          <v-btn class="ml-5" color="primary" height="56"> Buscar </v-btn>
         </v-col>
       </v-row>
     </v-card>
@@ -43,9 +35,9 @@
     <v-card class="ms-2 pa-3 tarjeta" elevation="8">
       <v-row align="center" class="pa-2" no-gutters>
         <v-col cols="8">
-          <p class="text-order"> Pedido N° {{ orderStore?.orders[0]?.id }} </p>
+          <p class="text-order">Pedido N° {{ orderStore?.orders[0]?.id }}</p>
         </v-col>
-        <v-col cols="4"> 
+        <v-col cols="4">
           <v-file-input
             hide-details
             accept="image/*"
@@ -58,43 +50,69 @@
       </v-row>
     </v-card>
     <div class="text-center mt-8">
-      <v-btn class="ms-2 my-6" align="center" color="primary" @click="onClick">
+      <v-btn
+        class="ms-2 my-6"
+        align="center"
+        color="primary"
+        @click="dialog = true"
+        height="56"
+      >
         Actualizar
       </v-btn>
-
     </div>
 
+    <template>
+      <div class="text-center pa-4">
+        <v-dialog v-model="dialog" width="auto">
+          <v-card
+            max-width="400"
+            prepend-icon="mdi-content-save"
+            text="La información del pedido ha sido actualizada exitosamente."
+            title="Pedido Actualizado"
+          >
+            <template v-slot:actions>
+              <RouterLink :to="{ name: 'searchOrder' }">
+                <v-btn
+                  class="ms-auto"
+                  text="Ok"
+                  @click="dialog = false"
+                ></v-btn>
+              </RouterLink>
+            </template>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
   </v-container>
 </template>
 
 <script>
-import { useRoute } from 'vue-router';
-import { useOrdersStore } from '../stores/Orders';
-import { onMounted } from 'vue';
+import { useRoute } from "vue-router";
+import { useOrdersStore } from "../stores/Orders";
+import { onMounted } from "vue";
 
 export default {
   data: () => ({
     loaded: false,
     loading: false,
+    dialog: false,
   }),
   setup() {
     const route = useRoute();
     const orderStore = useOrdersStore();
 
     if (orderStore?.orders[0]?.id !== route.params.id) {
-      onMounted( () => {
-        const id = route.params.id
+      onMounted(() => {
+        const id = route.params.id;
         if (id) {
-          orderStore.getOrders(id)
+          orderStore.getOrders(id);
         }
-      })
+      });
     }
 
     return {
-      orderStore
-    }
-
-
+      orderStore,
+    };
   },
   methods: {
     onClick() {
@@ -110,11 +128,8 @@ export default {
 </script>
 
 <style scoped>
-
 .text-order {
   font-size: 20px;
   font-weight: 600;
 }
-
-
 </style>
