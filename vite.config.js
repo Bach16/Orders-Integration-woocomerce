5b9a983c018a8b06114a59e3f0571b200123465b
Usuario <gwyn56717@gmail.com>
2024-06-12 17:07:48 -0500

    deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          vendor: ['vue']
+        }
+      }
+    }
+  },
+  server:{
+    historyApiFallback:true
+  }
 })
